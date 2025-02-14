9c7098e920cb5b73c8faf9346c67200a14f189e7
sergi <sergi@PC-de-Sergio>
2025-02-14 23:37:07 +0100

    Versión 3

---
+++ Proyecto2/src/paqueteRama2/Main.java
@@ -0,0 +1,10 @@
+package paqueteRama2;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Esta es la rama 2.");
+	}
+
+}
