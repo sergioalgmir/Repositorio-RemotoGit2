a41497010eaf462159b3c4e4ff4ce538a3d0746d
sergi <sergi@PC-de-Sergio>
2025-02-14 23:25:21 +0100

    Versión 2

---
+++ Proyecto2/src/paqueteRama1/Rama1.java
@@ -0,0 +1,12 @@
+package paqueteRama1;
+
+public class Rama1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Este es el primer mensaje.");
+		System.out.println("Este es el segundo mensaje");
+	}
+
+}
